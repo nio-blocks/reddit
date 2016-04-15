4b9fc4da14c68577ec922e9d9779d7114c4d23f6
Kylie Dale <kdale@MacBook-Pro.local>
2016-04-15 16:18:12 -0600

    initialize functions and tests

--- reddit_subreddit_block.py
+++ reddit_subreddit_block.py
@@ -1,14 +1,42 @@
+from .http_blocks.rest.rest_block import RESTPolling
 from nio.common.block.base import Block
 from nio.common.discovery import Discoverable, DiscoverableType
 from nio.metadata.properties import VersionProperty
 
 
 @Discoverable(DiscoverableType.block)
-class Example(Block):
+class SubredditFeed(RESTPolling):
 
-    """ This is the Example block. Put a brief description here. """
+    """ This block polls the Reddit feed for subreddits that match the queries.
+
+    Attributes:
+        queries (List(str)): Reddit subreddits to query.
+
+    """
 
     version = VersionProperty('0.1.0')
+
+    URL_FORMAT = ("https://www.reddit.com/r/{}.json")
+
+    # TODO: initialize queries attribute
+    # queries = ["nba"]
+
+    def __init__(self):
+        super().__init__()
+
+    def configure(self, context):
+        super().configure(context)
+
+    def start(self):
+        super().start()
+
+    def _prepare_url(self):
+        self.url = "https://www.reddit.com/r/nba.json"
+        # self.url = self.URL_FORMAT.format("nba")
+        print(self.url)
+        self._logger.info("No poll job")
+        return self.url
+        # self.url = self.URL_FORMAT.format(self.current_query)
 
     def process_signals(self, signals, input_id='default'):
         for signal in signals:
--- tests/test_reddit_subreddit_block.py
+++ tests/test_reddit_subreddit_block.py
@@ -1,15 +1,39 @@
 from collections import defaultdict
 from nio.common.signal.base import Signal
 from nio.util.support.block_test_case import NIOBlockTestCase
-from ..example_block import Example
+from ..reddit_subreddit_block import SubredditFeed
 
 
-class TestExample(NIOBlockTestCase):
+class TestSubredditFeed(NIOBlockTestCase):
 
     def setUp(self):
         super().setUp()
         # This will keep a list of signals notified for each output
         self.last_notified = defaultdict(list)
+
+    def test_prepare_url(self):
+        blk = SubredditFeed()
+        self.configure_block(blk, {
+            "queries": ["foobar"]
+        })
+
+        def prepare_url(blk, feed_type):
+            return blk.URL_FORMAT.format(blk.queries[0])
+        # default feed type
+        blk._prepare_url()
+        self.assertEqual(blk.url, prepare_url(blk, 'feed'))
+        # # posts
+        # blk.feed_type = FeedType.POSTS
+        # blk._prepare_url()
+        # self.assertEqual(blk.url, prepare_url(blk, 'posts'))
+        # # posts
+        # blk.feed_type = FeedType.TAGGED
+        # blk._prepare_url()
+        # self.assertEqual(blk.url, prepare_url(blk, 'tagged'))
+        # # posts
+        # blk.feed_type = FeedType.PROMOTABLE_POSTS
+        # blk._prepare_url()
+        # self.assertEqual(blk.url, prepare_url(blk, 'promotable_posts'))
 
     def signals_notified(self, signals, output_id='default'):
         self.last_notified[output_id].extend(signals)
@@ -18,7 +42,7 @@ class TestExample(NIOBlockTestCase):
         pass
 
     def test_process_signals(self):
-        blk = Example()
+        blk = SubredditFeed()
         self.configure_block(blk, {})
         blk.start()
         blk.process_signals([Signal()])
