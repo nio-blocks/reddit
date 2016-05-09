97e34639d97279a4ca398250bbeb42be9b69b069
Kylie Dale <kdale@MacBook-Pro.local>
2016-05-09 14:04:53 -0600

    move query param initialization into configure function

--- reddit_subreddit_block.py
+++ reddit_subreddit_block.py
@@ -37,21 +37,34 @@ class SubredditFeed(RESTPolling):
 
     creds = ObjectProperty(Creds, title='Credentials')
 
-    # TODO: initialize queries attribute
-    # queries = ["nba", "iot"]
-
     def __init__(self):
         super().__init__()
-        self.paging_marker_post_id = ""
+        self.post_ids = []
 
     def configure(self, context):
         super().configure(context)
+        self._logger.debug(self.queries)
+        for query in self.queries:
+            self._logger.debug("QUERY: " + query)
+            self.init_post_id(query);
+
+    def init_post_id(self, query):
+        self._logger.debug("inside init_post_id")
+        token = self.get_token();
+        headers = {"Authorization": "bearer " + token, "User-Agent": "nio"}
+        self._logger.debug(self._idx)
+        self._logger.debug(self.post_ids)
+        # TODO: set first post_id to none (we don't have a before query param)
+        response = requests.get(self.URL_FORMAT.format(query, None), headers=headers)
+        resp = response.json()
+        self._logger.debug("made it through get request")
+        self._logger.debug(resp['data']['children'][0]['data']['name'])
+        self.post_ids.append(resp['data']['children'][0]['data']['name'])
 
     def start(self):
         super().start()
 
     def get_token(self):
-        # self._logger.debug(self.creds.client_id)
         client_auth = requests.auth.HTTPBasicAuth(self.creds.client_id, self.creds.app_secret)
         post_data = {"grant_type": "password", "username": self.creds.app_username, "password": self.creds.app_password}
         headers = {"User-Agent": "nio"}
@@ -83,24 +96,9 @@ class SubredditFeed(RESTPolling):
         """
         token = self.get_token();
         headers = {"Authorization": "bearer " + token, "User-Agent": "nio"}
-        response = requests.get(self.URL_FORMAT.format(self.current_query, self.paging_marker_post_id), headers=headers)
+        response = requests.get(self.URL_FORMAT.format(self.current_query, self.post_ids[self._idx]), headers=headers)
         self.url = response.url
         self._logger.debug("********")
-        self._logger.debug(response.request.headers)
-        # self._logger.debug("POST ID " + self.paging_marker_post_id)
-
-        # if not paging:
-            # self.paging_url = None
-            # header['count'] = 25
-            # header['before'] = self.paging_marker_post_id
-
-        # else:
-            # self._logger.debug("PAGING")
-            # q_params = {"count": "25", "before": paging_marker_post_id}
-            # headers.update(q_params)
-            # self._logger.debug(headers)
-            # response = requests.get(self.URL_FORMAT.format(self.current_query), headers=headers)
-            # self.paging_url = response.url
 
         return headers
 
@@ -120,27 +118,23 @@ class SubredditFeed(RESTPolling):
         resp = resp.json()
         posts = resp['data']['children']
 
-        # self._logger.debug(self.page_num)
-        # if self.page_num > 1:
-        #     paging = True
         self._logger.debug("Reddit response contains %d posts" % len(posts))
-        self._logger.debug(paging)
 
         if posts:
-            self._logger.debug("$$$ Setting new post id $$$")
-            self._logger.debug("Old Post id: " + self.paging_marker_post_id)
-            self.paging_marker_post_id = self._get_post_id(posts[0])
-            self._logger.debug("Newest Post id: " + self.paging_marker_post_id)
+            # self._logger.debug("$$$ Setting new post id $$$")
+            # self._logger.debug("Old Post id: " + self.post_ids[self._idx])
+            self._logger.debug("###")
+            self._logger.debug(self.post_ids)
+            self._logger.debug(self._idx)
+            self.post_ids[self._idx] = self._get_post_id(posts[0])
+            self._logger.debug(self.post_ids[self._idx])
+            # self._logger.debug("Newest Post id: " + self.post_ids[self._idx])
         for post in posts:
+            self._logger.debug(SubredditSignal(post))
+            self._logger.debug(signals)
             signals.append(SubredditSignal(post))
         if len(posts) == 25:
+            self._logger.debug("Paging set to true")
             paging = True
 
         return signals, paging
-
-
-    # def process_signals(self, signals, input_id='default'):
-    #     self._logger.debug("processing signals")
-    #     for signal in signals:
-    #         pass
-    #     self.notify_signals(signals, output_id='default')
