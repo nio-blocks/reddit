0daff571258a6e05d7fb184abb9283fe40389259
mattdodge <matt@n.io>
2016-05-31 16:51:46 -0700

    Don't reobtain an access token on every request

--- tests/test_reddit_subreddit_block.py
+++ tests/test_reddit_subreddit_block.py
@@ -1,8 +1,6 @@
 from collections import defaultdict
-from nio.common.signal.base import Signal
 from nio.util.support.block_test_case import NIOBlockTestCase
 from ..reddit_subreddit_block import SubredditFeed
-from ..http_blocks.rest.rest_block import RESTPolling
 from unittest.mock import patch, MagicMock
 
 
@@ -14,10 +12,11 @@ class TestSubredditFeed(NIOBlockTestCase):
         self.last_notified = defaultdict(list)
 
     def test_authenticate(self):
-        """ Test that headers are properly set in _authenticate. """
+        """ Test that headers are properly set after _authenticate. """
         blk = SubredditFeed()
         blk.get_token = MagicMock(return_value="TEST TOKEN")
-        headers = blk._authenticate()
+        blk._authenticate()
+        headers = blk._get_headers()
         self.assertEqual(headers,
                          {'User-Agent': 'nio',
                           'Authorization': 'bearer TEST TOKEN'})
@@ -26,20 +25,20 @@ class TestSubredditFeed(NIOBlockTestCase):
         """ Test url has a before query param and appends proper subreddit. """
         blk = SubredditFeed()
         blk.init_post_id = MagicMock()
-        blk._authenticate = MagicMock()
+        blk.get_token = MagicMock(return_value="TEST TOKEN")
         blk.post_ids = ["RedditID"]
         self.configure_block(blk, {
             "queries": ["baz"]
         })
-        blk.poll()
+        blk._prepare_url(False)
         self.assertEqual(
             blk.url, "https://oauth.reddit.com/r/baz/new.json?before=RedditID")
 
     def test_process_response(self):
         """ Test that _process_response returns a single signal object. """
         blk = SubredditFeed()
-        blk._authenticate = MagicMock()
         blk.init_post_id = MagicMock()
+        blk.get_token = MagicMock(return_value="TEST TOKEN")
         blk.post_ids = ["RedditID"]
         self.configure_block(blk, {
             "queries": [
